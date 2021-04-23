880c5b70fe4a16253e55fc20731ee099c62aadc1
z_kyun <imkyun@163.com>
2021-04-23 23:03:17 +0800

    feat: 新增群组module

---
+++ k-im-chat-server/src/modules/group/entity/group.entity.ts
@@ -0,0 +1,36 @@
+import { Base } from 'src/common/entities/base.entity';
+import { Entity, Column } from 'typeorm';
+
+// 群组表
+@Entity()
+export class Group extends Base {
+  @Column({
+    comment: '群主id',
+  })
+  uid: string;
+
+  @Column({
+    comment: '群组名称',
+  })
+  groupName: string;
+
+  @Column({
+    comment: '公告',
+    default: '群主很懒, 没写公告',
+  })
+  notice: string;
+}
+
+// 群组 和 用户关联的中间表
+@Entity()
+export class GroupMap extends Base {
+  @Column({
+    comment: '群组id',
+  })
+  gid: string;
+
+  @Column({
+    comment: '用户id',
+  })
+  uid: string;
+}
